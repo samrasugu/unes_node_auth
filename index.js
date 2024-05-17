5c34f19a0cc62352d483b779be55a9d3a88b1c0f
samrasugu <mokuasamr@gmail.com>
2024-05-17 12:24:54 +0300

    feat: fetch json file
    - fetch json file from endpoint
    - convert json file to csv

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const defaultRouter = require("./routes/default");
+const fetchRouter = require("./routes/fetch");
 
 const PORT = process.env.PORT || 3000;
 const app = express();
@@ -9,6 +10,7 @@ const app = express();
 // Client -> Middleware -> Server -> Client
 app.use(express.json());
 app.use(defaultRouter);
+app.use(fetchRouter);
 
 app.listen(PORT, "0.0.0.0", () => {
     console.log(`Connected at Port: ${PORT}`);
---
+++ routes/fetch.js
@@ -0,0 +1,27 @@
+require("dotenv").config();
+const express = require("express");
+const fetchRouter = express.Router();
+const fetchData = require("../helpers/fetch");
+const convertSaveCSV = require("../helpers/convert_save");
+
+const endpoint = process.env.ENDPOINT_URL;
+const username = process.env.USERNAME;
+const password = process.env.PASSWORD;
+const filePath = './csvfile.csv';
+
+fetchRouter.get("/fetch_file", async (req, res) => {
+   try {
+    // get json file from endpoint
+     const jsonData = await fetchData(endpoint, username, password);
+    
+    //  convert json file to csv and save it locally
+     convertSaveCSV(jsonData, filePath);
+
+     res.send("JSON file converted to csv successfully. File at ./csvfile.csv");
+   } catch (error) {
+    console.log(`Error occurred while converting json file to csv ${error.message}`);
+    res.status(500).send("An error occurred");
+   }
+});
+
+module.exports = fetchRouter;
